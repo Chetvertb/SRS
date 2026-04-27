b741b81d942c0470001052175e222dfb2fed41db
Chetvertb <renovaciomaks@gmail.com>
2026-04-27 17:07:16 +0300

    first file

---
+++ utils.py
@@ -0,0 +1,11 @@
+import json
+
+
+def load_cards(filename='cards.json'):
+    with open(filename, 'r', encoding='utf-8')as f:
+        return json.load(f)
+
+
+def save_cards(data, filename='cards1.json'):
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
